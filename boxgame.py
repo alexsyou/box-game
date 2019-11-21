272a938605c3beac37849ad46f55270fad4f5300
alexsyou <youalex@udel.edu>
2019-11-20 22:38:15 -0500

    Finished menu window and added wall and game_view

--- boxgame.py
+++ boxgame.py
@@ -4,9 +4,32 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 TITLE = "BoxGame"
 
+class Wall(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__("images/wall.png")
+        self.center_x=x
+        self.center_y=y
 class MenuView(arcade.View):
     def on_show(self):
         arcade.set_background_color(arcade.color.WHITE)
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Welcome to BOXGAME", SCREEN_WIDTH/2, SCREEN_HEIGHT/2, arcade.color.GREEN_YELLOW, font_size=70, anchor_x="center")
+        arcade.draw_text("Use a mouse press to begin", SCREEN_WIDTH/2, SCREEN_HEIGHT*.30, arcade.color.GO_GREEN, font_size=40, anchor_x="center")
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        self.window.show_view(game_view)
+
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+
+        self.time_taken=0
+        self.box_list = arcade.SpriteList
+        self.wall_list = arcade.SpriteList
+        self.finish_list = arcade.SpriteList
+
+
 
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
